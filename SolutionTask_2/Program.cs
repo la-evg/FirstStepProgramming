dadcc2954e4601adc51ac2fd8f7cab238366696d
Tema_Larin <artem_larin@yoniti.ru>
2022-07-09 19:47:17 +0300

    add SolutionTask_2

---
+++ SolutionTask_2/Program.cs
@@ -0,0 +1,16 @@
+﻿var week = new Dictionary<int, string>()
+{
+    { 1, "Понедельник"},
+    { 2, "Вторник"},
+    { 3, "Среда"},
+    { 4, "Четверг"},
+    { 5, "Пятница"},
+    { 6, "Суббота"},
+    { 7, "Воскресенье"}
+};
+
+string? inputNum = Console.ReadLine();
+if(inputNum != null)
+{
+    Console.WriteLine(week[int.Parse(inputNum)]);
+}
