2a31801f418b6d34b6920cef14043c39411df009
Tema_Larin <artem_larin@yoniti.ru>
2022-07-09 18:33:02 +0300

    add SolutionTask_0

---
+++ SolutionTask_0/Program.cs
@@ -0,0 +1,11 @@
+﻿string? InputLine = Console.ReadLine();
+
+if(InputLine != null)
+{
+    int inputNumber = int.Parse(InputLine);
+    //int outNumber = inputNumber * inputNumber;
+
+    int outNumber = (int)Math.Pow(inputNumber,2);
+
+    Console.WriteLine(outNumber);
+}
