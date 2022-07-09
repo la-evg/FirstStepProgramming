775550a97cdf952365e3886cce2280b6633f78c3
Tema_Larin <artem_larin@yoniti.ru>
2022-07-09 19:16:52 +0300

    add SolutionTask_1

---
+++ SolutionTask_1/Program.cs
@@ -0,0 +1,17 @@
+﻿String? InputOneNum = Console.ReadLine();
+String? InputTwoNum = Console.ReadLine();
+
+if(InputOneNum != null && InputTwoNum != null)
+{
+    int OneNum = int.Parse(InputOneNum);
+    int TwoNum = int.Parse(InputTwoNum);
+
+    if(TwoNum == Math.Sqrt(OneNum))
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
