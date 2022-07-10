ad969be5d18089b3311bff2d0b3c8d8c3ccabfd1
Tema_Larin <artem_larin@yoniti.ru>
2022-07-10 16:02:19 +0300

    add SolutionTask_5

---
+++ SolutionTask_5/Program.cs
@@ -0,0 +1,14 @@
+﻿string? InputFirstNumber = Console.ReadLine();
+string? InputSecondNumber = Console.ReadLine();
+
+if(InputFirstNumber != null && InputSecondNumber != null)
+{
+    if(int.Parse(InputFirstNumber) < int.Parse(InputSecondNumber))
+    {
+        Console.WriteLine(InputSecondNumber);
+    }
+    else
+    {
+        Console.WriteLine(InputFirstNumber);
+    }
+}
