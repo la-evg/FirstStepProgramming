c43cd706956920e80c02b29011676a2ce7fca37d
Tema_Larin <artem_larin@yoniti.ru>
2022-07-10 16:18:19 +0300

    add SolutionTask_6

---
+++ SolutionTask_6/Program.cs
@@ -0,0 +1,23 @@
+﻿string? InputFirstNumber = Console.ReadLine();
+string? InputSecondNumber = Console.ReadLine();
+string? InputThirdNumber = Console.ReadLine();
+
+if(InputFirstNumber != null && InputSecondNumber != null && InputThirdNumber != null)
+{
+    if(int.Parse(InputFirstNumber) > int.Parse(InputSecondNumber)){
+        if(int.Parse(InputFirstNumber) > int.Parse(InputThirdNumber)){
+            Console.WriteLine(InputFirstNumber);
+        }
+        else{
+            Console.WriteLine(InputThirdNumber);
+        }
+    }
+    else{
+        if(int.Parse(InputSecondNumber) > int.Parse(InputThirdNumber)){
+            Console.WriteLine(InputSecondNumber);
+        }
+        else{
+            Console.WriteLine(InputThirdNumber);
+        }
+    }
+}
