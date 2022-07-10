ea523c727a991e3edd126e66096ddb6a40c71c3e
Tema_Larin <artem_larin@yoniti.ru>
2022-07-10 15:54:32 +0300

    add SolutionTask_4

---
+++ SolutionTask_4/Program.cs
@@ -0,0 +1,8 @@
+﻿string? InputNumber = Console.ReadLine();
+
+if(InputNumber != null)
+{
+    string LastNum = InputNumber.Substring(InputNumber.Length-1);
+
+    Console.WriteLine(int.Parse(LastNum));
+}
