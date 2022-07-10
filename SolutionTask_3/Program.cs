b8b32566f97da12dfa9efdf87b26e4587af97cf8
Tema_Larin <artem_larin@yoniti.ru>
2022-07-10 15:29:40 +0300

    add SolutionTask_3

---
+++ SolutionTask_3/Program.cs
@@ -0,0 +1,16 @@
+﻿string? InputNumber = Console.ReadLine();
+
+if(InputNumber != null)
+{
+    int InputNum = int.Parse(InputNumber);
+    int StartNumber = 1;
+    string LineOutput = "0";
+
+    while (StartNumber <= InputNum)
+    {
+        LineOutput = StartNumber * (-1) + ", "+LineOutput+", " + StartNumber;
+        StartNumber++;
+    }
+
+    Console.WriteLine(LineOutput);
+}
