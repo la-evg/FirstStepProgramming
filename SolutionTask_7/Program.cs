3a2b6818e6779c8ec667aeb99cbe742d839d1e76
Tema_Larin <artem_larin@yoniti.ru>
2022-07-10 17:09:51 +0300

    add SolutionTask_7

---
+++ SolutionTask_7/Program.cs
@@ -0,0 +1,13 @@
+﻿String? InputNum = Console.ReadLine();
+
+if(InputNum != null)
+{
+    if((int.Parse(InputNum) % 2) == 0)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
