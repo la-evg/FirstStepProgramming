19074b43c5a04f69f67f0b40f660041c12c6c2c9
Tema_Larin <artem_larin@yoniti.ru>
2022-07-17 18:11:11 +0300

    add homework SolutionTask_13

---
+++ SolutionTask_13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+void SearchThirdNumber()
+{
+    Console.Write("Введите число: ");
+    string? inputLine = Console.ReadLine();
+    if (inputLine != null)
+    {
+        int inputNumber = Math.Abs(int.Parse(inputLine));
+        if (inputNumber.ToString().Length <= 2)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else
+        {
+            char[] numArr = inputNumber.ToString().ToCharArray();
+            Console.Write("Третья цифра = " + numArr[2]);
+        }
+    }
+}
+SearchThirdNumber();
