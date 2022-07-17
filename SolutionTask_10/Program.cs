c7bbff45ff8aa1d8c1a4179bbd7820daffa3cb74
Tema_Larin <artem_larin@yoniti.ru>
2022-07-17 18:00:09 +0300

    add homework SolutionTask_10

---
+++ SolutionTask_10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+void SearchSecondNumber()
+{
+    Console.Write("Введите трёхзначное число: ");
+    string? inputLine = Console.ReadLine();
+    if (inputLine != null)
+    {
+        int inputNumber = Math.Abs(int.Parse(inputLine));
+        if (inputNumber.ToString().Length != 3)
+        {
+            Console.WriteLine("Это не трехзначное число. Попробуй снова");
+            SearchSecondNumber();
+        }
+        else
+        {
+            char[] numArr = inputNumber.ToString().ToCharArray();
+            Console.Write("Вторая цифра = ");
+            Console.Write(numArr[1]);
+        }
+    }
+}
+SearchSecondNumber();
