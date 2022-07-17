684b09e60c3e6533513b313f9d61235becc0eb5b
Tema_Larin <artem_larin@yoniti.ru>
2022-07-17 18:37:44 +0300

    add homework SolutionTask_15

---
+++ SolutionTask_15/Program.cs
@@ -0,0 +1,29 @@
+﻿// Является ли этот день выходным
+Console.Clear();
+void weekend()
+{
+    Console.Write("Введите число дня недели: ");
+    string? dayLine = Console.ReadLine();
+    if (dayLine != null)
+    {
+        int dayNumber = int.Parse(dayLine);
+        if (dayNumber < 1 ^ dayNumber > 8)
+        {
+            Console.WriteLine("Число от от 1 до 7");
+            weekend();
+        }
+        else
+        {
+            if (dayNumber == 6 ^ dayNumber == 7)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
+
+weekend();
