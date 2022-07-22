3f8771cde8db840a4479545584546dd382c61c18
Tema_Larin <artem_larin@yoniti.ru>
2022-07-22 16:56:52 +0300

    add SolutionTask_22

---
+++ SolutionTask_22/Program.cs
@@ -0,0 +1,57 @@
+﻿//вариант 1
+
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+
+    string lineN = "";
+    string lineNN = string.Empty;
+    int s = 1;
+
+    while(s <= numberN)
+    {
+        lineN = lineN + s +  " ";
+        lineNN  = lineNN + (s * s).ToString() + " ";
+        s++;
+    }
+
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
+
+// Вариант Кирилла
+// List<int> listGen(int number)
+// {
+//     List<int> numbers = new List<int>();
+
+//     for(int i = 1; i <= number; i++)
+//     {
+//         numbers.Add(i);
+//     }
+
+//     return numbers;
+// }
+
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+//     List<int> nums = listGen(inputNumber);
+
+//     foreach (var i in nums)
+//     {
+//         Console.Write(i);
+//         Console.Write(" ");
+//     }
+
+//     Console.WriteLine();
+
+//     foreach (var i in nums)
+//     {
+//         Console.Write(Math.Pow(i, 2));
+//         Console.Write(" ");
+//     }
+// }
