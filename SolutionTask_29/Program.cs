e5211e99a99958ccac34df3280c2c49d638ff6c7
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 22:07:12 +0300

    add SolutionTask_29 Homework

---
+++ SolutionTask_29/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Random numberSintezator = new Random();
+void VariantNative()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < 8 - 1)
+    {
+
+        Console.Write(numberSintezator.Next(0,19) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,19));
+    Console.Write("]");
+}
+VariantNative();
