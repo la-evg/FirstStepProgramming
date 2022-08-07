16856c7579860029b47c9675bdd720d57a56e139
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 23:08:23 +0300

    add SolutionTask_25(additionally) Homework

---
+++ SolutionTask_25(additionally)/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.Clear();
+
+//Обьявляем переменных
+int inputFirstNumber;
+string inputChar;
+int inputSecondNumber;
+int result;
+
+void ReadNumbers()
+{
+    //Принимаем первое число
+    Console.Write("Введите первое число: ");
+    inputFirstNumber = int.Parse(Console.ReadLine());
+
+    //Принимаем знак операции
+    Console.Write("Введите знак вычисления: ");
+    inputChar = Console.ReadLine();
+
+    //Принимаем второе число
+    Console.Write("Введите второе число: ");
+    inputSecondNumber = int.Parse(Console.ReadLine());
+}
+
+//Описываем инструкцию каждой операции
+int Sum()
+{
+    result = inputFirstNumber + inputSecondNumber;
+    return result;
+}
+
+int Difference()
+{
+    result = inputFirstNumber - inputSecondNumber;
+    return result;
+}
+
+int Multiplication()
+{
+    result = inputFirstNumber * inputSecondNumber;
+    return result;
+}
+
+int Division()
+{
+    result = inputFirstNumber / inputSecondNumber;
+    return result;
+}
+
+int Remainder()
+{
+    result = inputFirstNumber % inputSecondNumber;
+    return result;
+}
+
+//Проверяем знак операции и вызываем соответствующий метод
+void Calculate()
+{
+    if (inputChar == "+")
+    {
+        Console.WriteLine($"{inputFirstNumber} {inputChar} {inputSecondNumber} = {Sum()}");
+    }
+    if (inputChar == "-")
+    {
+        Console.WriteLine($"{inputFirstNumber} {inputChar} {inputSecondNumber} = {Difference()}");
+    }
+    if (inputChar == "*")
+    {
+        Console.WriteLine($"{inputFirstNumber} {inputChar} {inputSecondNumber} = {Multiplication()}");
+    }
+    if (inputChar == "/")
+    {
+        Console.WriteLine($"{inputFirstNumber} {inputChar} {inputSecondNumber} = {Division()}");
+    }
+    if (inputChar == "%")
+    {
+        Console.WriteLine($"{inputFirstNumber} {inputChar} {inputSecondNumber} = {Remainder()}");
+    }
+}
+
+//Вызов методов
+ReadNumbers();
+Calculate();
