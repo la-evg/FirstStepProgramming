9109a243d576361312b435dbbd3c64502feeb9c6
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 15:37:25 +0300

    add SolutionTask_30

---
+++ SolutionTask_30/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Random numberSintezator = new Random();
+
+void VariantNative()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < 8 - 1)
+    {
+
+        Console.Write(numberSintezator.Next(0,2) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,2));
+    Console.Write("]");
+}
+
+VariantNative();
