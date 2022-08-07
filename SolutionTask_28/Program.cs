35e6603ef47f185c2a56849acb4c425748432d59
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 15:14:09 +0300

    add SolutionTask_28

---
+++ SolutionTask_28/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+int t;
+
+t = Environment.TickCount;
+Console.WriteLine(sumNums(inputNum));
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+
+t = Environment.TickCount;
+Console.WriteLine(mulRec(inputNum));
+Console.WriteLine("Recurs time: {0} ms", Environment.TickCount - t);
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+int mulRec(int num) 
+{
+    if (num == 1) {
+        return 1;
+    } else {
+        return num * mulRec(num - 1);
+    }
+}
