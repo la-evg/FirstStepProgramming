631d189672031d2e38f6a2b23c11a6f669e49083
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 21:53:21 +0300

    add SolutionTask_27 Homework

---
+++ SolutionTask_27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());
+int sum = 0;
+
+int CalculateSum()
+{
+    while(inputNumber > 10){
+        sum = sum + inputNumber % 10;
+        inputNumber /= 10;
+    }
+    sum += inputNumber;
+    return sum;
+}
+Console.WriteLine($"Сумма чисел в {inputNumber} равна {CalculateSum()}");
