d35f8a6ec948dba36a3b5b6eee0d67aa010d465a
Tema_Larin <artem_larin@yoniti.ru>
2022-08-07 15:02:25 +0300

    add SolutionTask_26

---
+++ SolutionTask_26/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Введите число: ");
+
+string inputLineNumber = Console.ReadLine() ?? "";
+int inputNumber = int.Parse(inputLineNumber);
+
+DateTime timePoint = DateTime.Now;
+
+int VariantChar()
+{
+    //int numberLength = 0;
+    //char[] array = inputLineNumber.ToCharArray();
+    //numberLength = array.Length;
+    return inputLineNumber.ToCharArray().Length;
+}
+
+int VariantSimple()
+{
+    int numberLenght = 0;
+    int digits = 1;
+    while (digits < inputNumber)
+    {
+        digits = digits * 10;
+        numberLenght++;
+    }
+    return numberLenght;
+}
+
+int VariantLog10()
+{
+    return (int)Math.Log10(inputNumber) + 1;
+}
+
+int result = 0;
+
+timePoint = DateTime.Now;
+result = VariantChar();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+
+timePoint = DateTime.Now;
+result = VariantSimple();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+result = VariantLog10();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
