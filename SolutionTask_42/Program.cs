96e95856a36e8dddd1763cf0faab41d0aa0f8d0f
Tema_Larin <artem_larin@yoniti.ru>
2022-08-15 17:15:57 +0300

    add SolutionTask_42

---
+++ SolutionTask_42/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string numbe1 = Console.ReadLine();
+
+DateTime d1 = DateTime.Now;
+Console.WriteLine(Convert.ToString(int.Parse(numbe1),2));
+Console.WriteLine(DateTime.Now-d1);
+
+d1 = DateTime.Now;
+Console.Write("Введите десятичное число: ");
+int decemalNumber = ReadDecemalNumber();
+PrintAnswer(BinaryConverter(decemalNumber));
+Console.WriteLine(DateTime.Now-d1);
+
+
+int ReadDecemalNumber(){
+    return int.Parse(numbe1 ?? "");
+}
+
+string BinaryConverter(int num) {
+    string bin = "";
+    while(num > 0) {
+        if (num % 2 == 1) {
+            bin = "1" + bin;
+        } else {
+            bin = "0" + bin;            
+        }
+        num /= 2;
+    }
+    return bin;
+}
+
+void PrintAnswer(string answer){
+    Console.WriteLine("Введенное число в двоичном виде: " + answer);
+}
