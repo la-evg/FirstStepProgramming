9ec9bf0161e899d98e60d6773fb111d010582dab
Tema_Larin <artem_larin@yoniti.ru>
2022-08-18 18:03:10 +0300

    add Solutiontask_48

---
+++ SolutionTask_48/Program.cs
@@ -0,0 +1,97 @@
+﻿//=========================================================================================================
+// Задайте двумерный массив размером m×n, каждый элемент массива находится по формуле m+n
+//=========================================================================================================
+
+// метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+//метод для печати двумерного массива
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        Console.Write("[");
+        while (j < inputArray.GetLength(1) - 1)
+        {
+            Console.Write(inputArray[i, j] + ", ");
+            j++;
+        }
+        //Console.Write("\n");
+        Console.Write(inputArray[i, j]);
+        Console.Write("]");
+        Console.WriteLine();
+        i++;
+    }
+}
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+void PrintColorTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+        Console.Write("[");
+        while (j < inputArray.GetLength(1) - 1)
+        {
+            Console.Write(inputArray[i, j] + ", ");
+            j++;
+        }
+        Console.Write(inputArray[i, j]);
+        Console.Write("]");
+        //Console.Write("\n");
+        Console.WriteLine();
+        i++;
+    }
+    Console.ResetColor();
+
+}
+
+void UpdateToDimArray(ref int[,] inputArray)
+{
+    int i = 0; int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            inputArray[i, j] = i + j;
+            j++;
+        }
+        i++;
+    }
+}
+
+int[,] twoDimArray = FillTwoDimArray(5, 8);
+
+PrintColorTwoDimArray(twoDimArray);
+Console.WriteLine();
+UpdateToDimArray(ref twoDimArray);
+PrintColorTwoDimArray(twoDimArray);
